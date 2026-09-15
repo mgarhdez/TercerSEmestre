a511cd3c06ed4eba9433500e539495069d092d10
mgarhdez <miguelgarciahernandez457@gmail.com>
2026-09-14 22:40:32 -0600

    no message

---
+++ prog05.py
@@ -0,0 +1,44 @@
+
+# Estructuras repetitivas 
+
+#a = "hola mundo"
+
+#for i in a:
+ #   print(i)
+
+#for i in range(1,5):
+ #   print(i)
+
+#Ciclo condicional 
+#a = 1
+#while(a<5):
+ #   print("Hola mundo")
+   # a = + 1
+ #   a += 1
+
+
+#Hacer un programa que lea 10 numeros y que muestre en  pantalla la sema de ellos
+
+# con for
+#suma = 0
+#for i in range(1,11):
+  #  a = int(input('Escribe un numero \n'))
+ #   suma += a 
+
+#print(f'la suma de los numeros son {suma}')
+
+n = 1
+suma = 0
+while(n <= 5):
+    a = int(input('Escibe un numero\n'))
+    suma += a 
+    n += 1
+print(f'la suma de los numeros son {suma}')
+
+
+
+
+
+
+
+    
